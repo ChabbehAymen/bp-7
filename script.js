6c993132165908f638ae3c85a769f6d3d63bfece
ChabbehAymen <chebbehaymen@gmail.com>
2023-11-16 16:17:22 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-// let xhr = new XMLHttpRequest();
-// xhr.responseType = 'json'
-// xhr.open('GET','/data/resturants_data.json');
+let xhr = new XMLHttpRequest();
+xhr.responseType = 'json'
+xhr.open('GET','/data/resturants_data.json');
 
-// xhr.onload = function() {
-//     console.log(xhr.response);
-// }
-// xhr.send();
-// console.log('hello');
+xhr.onload = function() {
+    console.log(xhr.response);
+}
+xhr.send();
 
-fetch('/data/resturants_data.json')
-    .then(res => res.json())
+// fetch('/data/resturants_data.json')
+//     .then(res => res.json())
 
-    .then(data => {
-        console.log(data[0].name)
-    })
+//     .then(data => {
+//         console.log(data[0].name)
+//     })
