6338207094d22d4074e66ebc2a11bc02e90ba05d
devaymen15.5.3@gmail.com <devaymen15.5.3@gmail.com>
2023-11-18 00:21:11 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-let allRestos = [];
+/*
+ *
+ */
+let rateRadios = document.querySelectorAll('input[name="rate-radio-group"]');
 let xhr = new XMLHttpRequest();
-xhr.responseType = "json";
 xhr.open("GET", "/data/resturants_data.json");
 
 xhr.onload = function () {
-  for (let resto of xhr.response) {
-    allRestos.push(resto);
-  }
+  let allRestos = JSON.parse(xhr.responseText);
+  createRestos(allRestos);
+  filterByRate(allRestos);
+
 };
 xhr.send();
 
-for (let resto of allRestos) {
-  document
-    .querySelector("main")
-    .appendChild(
-      createRestoCard(
-        resto.id,
-        resto.logo,
-        resto.name,
-        resto.rate,
-        resto.speciality
-      )
-    );
+function createRestos(allRestos) {
+  document.querySelector("main").innerHTML = "";
+  for (let resto of allRestos) {
+    document
+      .querySelector("main")
+      .appendChild(
+        createRestoCard(
+          resto.id,
+          resto.logo,
+          resto.name,
+          resto.note,
+          resto.specialty
+        )
+      );
+  }
 }
 
 function createRestoCard(id, logo, name, rate, speciality) {
@@ -33,3 +39,92 @@ function createRestoCard(id, logo, name, rate, speciality) {
   restoCard.setAttribute("speciality", speciality);
   return restoCard;
 }
+function filterByRate(allRestos) {
+  rateRadios.forEach((radio) => {
+    radio.onclick = function () {
+      switch (radio.value) {
+        case radioStates.allRestos:
+          presentAllRestos(allRestos);
+          break;
+        case radioStates.lessThanTree:
+          filterLessOfTreeRateRestos(allRestos);
+          break;
+        case radioStates.threeToFour:
+          filterFormThreeToFourRate(allRestos);
+          break;
+        case radioStates.fourToFive:
+          filterFormFourToFiveRate(allRestos);
+          break;
+        default:
+          break;
+      }
+    };
+  });
+}
+function presentAllRestos(allRestos) {
+  createRestos(allRestos);
+}
+
+// filters resturants to only have rate less than 3
+function filterLessOfTreeRateRestos(allRestos) {
+  let main = document.querySelector("main");
+  main.innerHTML = "";
+  for (const resto of allRestos) {
+    if (resto.note < 3) {
+      main.appendChild(
+        createRestoCard(
+          resto.id,
+          resto.logo,
+          resto.name,
+          resto.note,
+          resto.specialty
+        )
+      );
+    }
+  }
+}
+
+// filters resturants to only have rate bettween 3 and 4
+function filterFormThreeToFourRate(allRestos) {
+  let main = document.querySelector("main");
+  main.innerHTML = "";
+  for (const resto of allRestos) {
+    if (resto.note > 3 && resto.note <= 4) {
+      main.appendChild(
+        createRestoCard(
+          resto.id,
+          resto.logo,
+          resto.name,
+          resto.note,
+          resto.specialty
+        )
+      );
+    }
+  }
+}
+
+// filter the resturants to only have rate above 4
+function filterFormFourToFiveRate(allRestos) {
+  let main = document.querySelector("main");
+  main.innerHTML = "";
+  for (const resto of allRestos) {
+    if (resto.note > 4 && resto.note <= 5) {
+      main.appendChild(
+        createRestoCard(
+          resto.id,
+          resto.logo,
+          resto.name,
+          resto.note,
+          resto.specialty
+        )
+      );
+    }
+  }
+}
+// this object represents the 4 states of my radio buttons to filtter by
+const radioStates = {
+  allRestos: "all",
+  lessThanTree: "-3",
+  threeToFour: "3-4",
+  fourToFive: "4-5",
+};
