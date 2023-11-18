377ccd42a0091eac6602610082c97257ef86820d
devaymen15.5.3@gmail.com <devaymen15.5.3@gmail.com>
2023-11-18 22:43:59 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
  *
  */
 let rateRadios = document.querySelectorAll('input[name="rate-radio-group"]');
+let main = document.querySelector("main");
 let xhr = new XMLHttpRequest();
 xhr.open("GET", "/data/resturants_data.json");
 
@@ -9,7 +10,9 @@ xhr.onload = function () {
   let allRestos = JSON.parse(xhr.responseText);
   createRestos(allRestos);
   filterByRate(allRestos);
-
+  createSpecialitySelect();
+  filterBySpeciality(allRestos);
+  searchInRestos();
 };
 xhr.send();
 
@@ -30,6 +33,7 @@ function createRestos(allRestos) {
   }
 }
 
+// creates my custom element for each object of the fetched json data
 function createRestoCard(id, logo, name, rate, speciality) {
   let restoCard = document.createElement("resto-card");
   restoCard.setAttribute("id", id);
@@ -39,12 +43,44 @@ function createRestoCard(id, logo, name, rate, speciality) {
   restoCard.setAttribute("speciality", speciality);
   return restoCard;
 }
+
+function createSpecialitySelect() {
+  let restos = document.querySelectorAll("main resto-card");
+  for (let resto of restos) {
+    let speciality = resto.getAttribute("speciality");
+    if (!isOptionCreated(speciality)) {
+      insertOption(speciality);
+    }
+  }
+}
+
+/*
+ this function checks whether the passed speciality allredy have a 
+ created option in the select
+*/
+function isOptionCreated(value) {
+  let createdOptions = document.querySelectorAll("header select option");
+  for (const option of createdOptions) {
+    if (value == option.value) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function insertOption(speciality) {
+  document.querySelector("header select").innerHTML += `
+  <option value="${speciality}">${speciality}</option>
+  `;
+}
+
 function filterByRate(allRestos) {
   rateRadios.forEach((radio) => {
     radio.onclick = function () {
+      document.querySelector("header select").value = "all-speciality";
       switch (radio.value) {
         case radioStates.allRestos:
-          presentAllRestos(allRestos);
+          createRestos(allRestos);
           break;
         case radioStates.lessThanTree:
           filterLessOfTreeRateRestos(allRestos);
@@ -61,13 +97,9 @@ function filterByRate(allRestos) {
     };
   });
 }
-function presentAllRestos(allRestos) {
-  createRestos(allRestos);
-}
 
 // filters resturants to only have rate less than 3
 function filterLessOfTreeRateRestos(allRestos) {
-  let main = document.querySelector("main");
   main.innerHTML = "";
   for (const resto of allRestos) {
     if (resto.note < 3) {
@@ -86,7 +118,6 @@ function filterLessOfTreeRateRestos(allRestos) {
 
 // filters resturants to only have rate bettween 3 and 4
 function filterFormThreeToFourRate(allRestos) {
-  let main = document.querySelector("main");
   main.innerHTML = "";
   for (const resto of allRestos) {
     if (resto.note > 3 && resto.note <= 4) {
@@ -105,7 +136,6 @@ function filterFormThreeToFourRate(allRestos) {
 
 // filter the resturants to only have rate above 4
 function filterFormFourToFiveRate(allRestos) {
-  let main = document.querySelector("main");
   main.innerHTML = "";
   for (const resto of allRestos) {
     if (resto.note > 4 && resto.note <= 5) {
@@ -121,6 +151,53 @@ function filterFormFourToFiveRate(allRestos) {
     }
   }
 }
+
+function filterBySpeciality(allRestos) {
+  document
+    .querySelector("header select")
+    .addEventListener("change", (event) => {
+      document.querySelector("#all-restos").checked = true;
+      filterRestos(allRestos, event.target.value);
+    });
+}
+
+function filterRestos(allRestos, speciality) {
+  main.innerHTML = "";
+  for (const resto of allRestos) {
+    if (speciality == "all-speciality") {
+      createRestos(allRestos);
+    } else if (resto.specialty == speciality) {
+      main.appendChild(
+        createRestoCard(
+          resto.id,
+          resto.logo,
+          resto.name,
+          resto.note,
+          resto.specialty
+        )
+      );
+    }
+  }
+}
+
+function searchInRestos() {
+  document.querySelector("#search-bar").addEventListener("keyup", (event) => {
+    document.querySelectorAll("resto-card").forEach((restoCard) => {
+      if (
+        restoCard.getAttribute("name").includes(event.target.value) &&
+        event.target.value != ""
+      ) {
+        restoCard.style.display = "block";
+      } else if (
+        !restoCard.getAttribute("name").includes(event.target.value) &&
+        event.target.value != ""
+      )
+        restoCard.style.display = "none";
+      if (event.target.value == '') restoCard.style.display = "block";
+    });
+  });
+}
+
 // this object represents the 4 states of my radio buttons to filtter by
 const radioStates = {
   allRestos: "all",
@@ -128,3 +205,5 @@ const radioStates = {
   threeToFour: "3-4",
   fourToFive: "4-5",
 };
+
+// TODO sorts The element depending on there rate in the page
